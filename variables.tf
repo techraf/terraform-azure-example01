6912774e64dc4ad29d885b14043095f94a8372a3
techraf <techraf@gmail.com>
2017-02-23 12:41:24 +0900

    Parametrise azurerm_subnet

--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@ variable "azure_resource_group_location" { default = "South Central US" }
 
 variable "azurerm_virtual_machine_size" { default = "Standard_A1" }
 variable "azurerm_virtual_machine_name" { default = "terra01vm" }
+
+variable "azurerm_subnet_name" { default = "terra01sub" }
+variable "azurerm_subnet_address_prefix" { default = "10.0.2.0/24" }
