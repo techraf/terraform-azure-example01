9fed68fb59f53c1e8edbf11dca5ce6f3357ed209
techraf <techraf@gmail.com>
2017-02-23 12:37:47 +0900

    Simplify variable definitions

--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
-variable "azure_resource_group_name" {
-    description = "Resource Group Name"
-    default = "terra01rg"
-}
+variable "azure_resource_group_name" { default = "terra01rg" }
+variable "azure_resource_group_location" { default = "South Central US" }
 
-variable "azure_resource_group_location" {
-    description = "Resource Group Location"
-    default = "South Central US"
-}
-
-variable "azurerm_virtual_machine_size" {
-    description = "VM Size"
-    default = "Standard_A1"
-}
-
-variable "azurerm_virtual_machine_name" {
-    description = "VM Name"
-    default = "terra01vm"
-}
+variable "azurerm_virtual_machine_size" { default = "Standard_A1" }
+variable "azurerm_virtual_machine_name" { default = "terra01vm" }
