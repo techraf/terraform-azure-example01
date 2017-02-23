0056a60f80f3978bc776d86e69956cc38a985ce7
techraf <techraf@gmail.com>
2017-02-23 20:39:03 +0900

    Parametrise azurerm_virtual_machine

--- variables.tf
+++ variables.tf
@@ -7,9 +7,6 @@ variable "azurerm_storage_account_account_type" { default = "Standard_LRS" }
 variable "azurerm_storage_container_name" { default = "vhds" }
 variable "azurerm_storage_container_container_access_type" { default = "private" }
 
-variable "azurerm_virtual_machine_size" { default = "Standard_A1" }
-variable "azurerm_virtual_machine_name" { default = "terra01vm" }
-
 variable "azurerm_subnet_name" { default = "terra01sub" }
 variable "azurerm_subnet_address_prefix" { default = "10.0.2.0/24" }
 
@@ -23,3 +20,13 @@ variable "azurerm_public_ip_domain_name_label" { default = "terra01vm" }
 
 variable "azurerm_virtual_network_name" { default = "terra01vn" }
 variable "azurerm_virtual_network_address_space" { default = ["10.0.0.0/16"] }
+
+variable "azurerm_virtual_machine_name" { default = "terra01vm" }
+variable "azurerm_virtual_machine_size" { default = "Standard_A1" }
+variable "azurerm_virtual_machine_storage_image_reference_publisher" { default = "MicrosoftWindowsServer" }
+variable "azurerm_virtual_machine_storage_image_reference_offer" { default = "WindowsServer" }
+variable "azurerm_virtual_machine_storage_image_reference_sku" { default = "2016-Datacenter" }
+variable "azurerm_virtual_machine_storage_image_reference_version" { default = "latest" }
+# variable "azurerm_virtual_machine_os_profile_computer_name" { default = "${var.azurerm_virtual_machine_name}" }
+variable "azurerm_virtual_machine_os_profile_admin_username" { default = "terrastrator" }
+variable "azurerm_virtual_machine_os_profile_admin_password" { default = "UNSAFE**123!`#$%&**UNSAFE" }
