850e9e5a83a98bdc7ca7aa6dc5173080f3716f85
techraf <techraf@gmail.com>
2017-02-23 12:48:31 +0900

    Parametrise azurerm_public_ip

--- variables.tf
+++ variables.tf
@@ -10,3 +10,7 @@ variable "azurerm_subnet_address_prefix" { default = "10.0.2.0/24" }
 variable "azurerm_network_interface_name" { default = "terra01ni" }
 variable "azurerm_network_interface_ip_configuration_name" { default = "testconfiguration1" }
 variable "azurerm_network_interface_ip_configuration_private_ip_address_allocation" { default = "dynamic" }
+
+variable "azurerm_public_ip_name" { default = "acceptanceTestPublicIp1" }
+variable "azurerm_public_ip_public_ip_address_allocation" { default = "dynamic" }
+variable "azurerm_public_ip_domain_name_label" { default = "terra01vm" }
